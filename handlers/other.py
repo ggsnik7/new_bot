69e97326fe3eb005ea77d1e78b89d615ba6077be
Tim Gasnikov <tim_gasnikov@mail.ru>
2026-06-26 13:26:43 +0300

    new_update

--- handlers/other.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-
-other_router = Router()
-
-@other_router.message()
-async def send_answer(message: Message):
-    await message.answer(text="Не знаю, как реагировать на такое сообщение")
--- keyboards/test_inline_keybord.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-keyboard: list[list[InlineKeyboardButton]] = [
-    [InlineKeyboardButton(text='Дай мне цифру', callback_data='number')]
-]
-
-markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
